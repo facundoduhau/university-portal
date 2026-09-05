2c0822587bd7c75c55597b3bf11cd6773c373288
facundoduhau <duhaufacundo@gmail.com>
2026-09-04 22:13:59 -0300

    Dependencies, prettierrc, server health work

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
+import { configDotenv } from "dotenv";
 import express from "express";
+configDotenv();
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.SERVER_PORT_NUMBER ?? 3000;
+
+app.get("/", (_req, res) => {
+    res.send("Hello World!");
+});
 
 app.get("/health", (_req, res) => {
-    res.json({ status: "ok" });
+    res.status(200).json({ status: "OK" });
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server is running on port http://localhost:${port}`);
 });
